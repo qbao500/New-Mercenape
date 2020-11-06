a34655ce76a79d1576831296a2d0793cde2fd81d
StonegazeFIN <33735539+StonegazeFIN@users.noreply.github.com>
2020-11-06 17:40:12 +0200

    Camera updates
    Now the camera won't go higher than the level background top.

--- Assets/Scripts/Camera/cameraManager.cs
+++ Assets/Scripts/Camera/cameraManager.cs
@@ -6,36 +6,70 @@ using Cinemachine;
 public class cameraManager : MonoBehaviour
 {
     //Written by Ossi Uusitalo
-    public GameObject mainCam;
+    public GameObject mainCam, player;
     CinemachineVirtualCamera cineVirtual;
     CinemachineFramingTransposer cineFrame;
     public float bottom, zoomIn; // You need to manually type the Ortographic size (ZoomIn) and ScreenY(bottom) from the editor to set the minimum level of zoom.
     public float top, zoomOut;
+    public bool stopY = false;
+    public float maxHeight, minHeight; //This will the parameters on which the camera can move on the Y axis.
 
     private void Start()
     {
-        cineVirtual = mainCam.GetComponent<CinemachineVirtualCamera>();
-        if(cineVirtual != null)
+        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        if (player != null)
         {
-            Debug.Log("CineVirtual found");
-            
-            cineFrame = cineVirtual.GetCinemachineComponent<CinemachineFramingTransposer>();
-            if (cineFrame != null)
+            cineVirtual = mainCam.GetComponent<CinemachineVirtualCamera>();
+            cineVirtual.m_Follow = player.transform;
+            if (cineVirtual != null)
             {
-                Debug.Log("cineFrame found");
-                //Set the maximum parameters for the zoom out
-                top = cineFrame.m_ScreenY;
-                zoomOut = cineVirtual.m_Lens.OrthographicSize;
+                Debug.Log("CineVirtual found");
 
-                //Then we set the camera on the max zoom in
-                cineVirtual.m_Lens.OrthographicSize = zoomIn;
-                cineFrame.m_ScreenY = bottom;
+                cineFrame = cineVirtual.GetCinemachineComponent<CinemachineFramingTransposer>();
+                if (cineFrame != null)
+                {
+                    Debug.Log("cineFrame found");
+                    //Set the maximum parameters for the zoom out
+                    top = cineFrame.m_ScreenY;
+                    zoomOut = cineVirtual.m_Lens.OrthographicSize;
+
+                    //Then we set the camera on the max zoom in
+                    cineVirtual.m_Lens.OrthographicSize = zoomIn;
+                    cineFrame.m_ScreenY = bottom;
+                    minHeight = mainCam.transform.position.y;
+                }
             }
         }
     }
 
     private void Update()
     {
+        cineFrame.m_BiasY = -0.5f;
+        //This prevents the camera from going further up to peek over the actual level
+        if(stopY)
+        {
+            cineFrame.m_DeadZoneHeight = 2f;
+            cineFrame.m_SoftZoneHeight = cineFrame.m_DeadZoneHeight;
+            cineFrame.m_DeadZoneWidth = cineFrame.m_SoftZoneWidth + 0.05f;
+
+        } else
+        {
+            cineFrame.m_DeadZoneWidth = 0f;
+            cineFrame.m_DeadZoneHeight = 0;
+            cineFrame.m_SoftZoneHeight = 0.14f;
+            cineFrame.m_SoftZoneWidth = 0.05f;
+
+        }
+        //The triggers for stopY:
+        if(player.transform.position.y > maxHeight || cineVirtual.m_Lens.OrthographicSize >= zoomOut * 0.8f)
+        {
+            stopY = true;
+        } else
+        {
+            stopY = false;
+        }
+
+
         //The Screen Y axis and Ortographic size are controlled by the mouse wheel
         cineFrame.m_ScreenY -= Input.mouseScrollDelta.y;
         cineVirtual.m_Lens.OrthographicSize -= Input.mouseScrollDelta.y;
