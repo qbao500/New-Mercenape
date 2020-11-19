69045f90ca817c0e01f53e75a83f35ddc410f439
phamthuyet <54832854+phamthuyet@users.noreply.github.com>
2020-11-19 23:35:36 +0200

    thuyet nov 19 c1

---
+++ Assets/CutSence.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//Prototype made by thuyet to handle cutscenes
+public class CutSence : MonoBehaviour
+{
+    public int imageCount;
+    float startWaitTime=1f ;
+    float delta;
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+        imageCount = 0;
+        delta = startWaitTime - Time.deltaTime;
+        if (delta <= 0)
+        {
+            gameObject.transform.GetChild(imageCount).gameObject.SetActive(true);
+            print("hello");
+        }
+
+    }
+    
+    
+
+ 
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+                imageCount++;
+
+                if (imageCount < gameObject.transform.childCount)
+                {
+                gameObject.transform.GetChild(imageCount).gameObject.SetActive(true);
+                 }
+                if (imageCount > 0)
+                {
+                    gameObject.transform.GetChild(imageCount - 1).gameObject.SetActive(false);
+                }
+           
+        }
+        if(imageCount >= gameObject.transform.childCount || Input.GetKeyDown(KeyCode.Escape))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            int buildIndex = currentScene.buildIndex;
+            SceneManager.LoadScene(buildIndex+1);
+        }
+    } 
+
+}
---
+++ Assets/videoscript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+
+//Prototype made by thuyet to handle start video such as logo or trailer
+
+public class videoscript : MonoBehaviour
+{
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+
+
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+          Scene currentScene = SceneManager.GetActiveScene();
+            int buildIndex = currentScene.buildIndex;
+            SceneManager.LoadScene(buildIndex+1);
+    }
+}
