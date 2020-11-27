b88065cd11bc8dd4d8fbdb3cb9054a72efb7db36
phamthuyet <54832854+phamthuyet@users.noreply.github.com>
2020-11-27 16:28:27 +0200

    thuyet c7 nov 27

--- Assets/Scripts/Dialogue/PlayerSpeech.cs
+++ Assets/Scripts/Dialogue/PlayerSpeech.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 // Created by Arttu Paldán on 5.11.2020: 
@@ -8,6 +9,8 @@ public class PlayerSpeech: MonoBehaviour
 {
     GameObject bubbleObject;
     TextMeshProUGUI speechBubble;
+    public Image bubbleTalk;
+    public Sprite[] bubbleTalkArray;
 
     public string howToAttack, howToBlock, cameraScroll;
     public float startMessageWait, waitBetweenMessages;
@@ -26,6 +29,7 @@ public class PlayerSpeech: MonoBehaviour
     IEnumerator Wait(float time)
     {
         speechBubble.text = "";
+        bubbleTalk.enabled = false;
 
         yield return new WaitForSeconds(time);
 
@@ -35,25 +39,37 @@ public class PlayerSpeech: MonoBehaviour
     IEnumerator SpawnMessage(TextMeshProUGUI messageObject, string message1, string message2, string message3, float time)
     {
         messageObject.text = message1;
+        bubbleTalk.enabled = true;
+        bubbleTalk.sprite = bubbleTalkArray[0];
 
         yield return new WaitForSeconds(time);
 
         messageObject.text = "";
+        bubbleTalk.enabled = false;
+
 
         yield return new WaitForSeconds(time);
 
         messageObject.text = message2;
+        bubbleTalk.enabled = true;
+        bubbleTalk.sprite = bubbleTalkArray[1];
 
         yield return new WaitForSeconds(time);
 
         messageObject.text = "";
+        bubbleTalk.enabled = false;
+
 
         yield return new WaitForSeconds(time);
 
         messageObject.text = message3;
+        bubbleTalk.enabled = true;
+        bubbleTalk.sprite = bubbleTalkArray[2];
 
         yield return new WaitForSeconds(time);
 
         messageObject.text = "";
+        bubbleTalk.enabled = false;
+
     }
 }
