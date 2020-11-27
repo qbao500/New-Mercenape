02600d8d524ced1526a2e979602d3b73bd69f5b0
Bao <qbao500@gmail.com>
2020-11-27 16:29:33 +0200

    Bao C2 27.11.20
    Tooltip implemented

---
+++ Assets/Scripts/Tooltip/TooltipTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string content;
+    public string header;
+
+    private Coroutine coroutine;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        coroutine = StartCoroutine(ShowToolTip());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (coroutine != null) { StopCoroutine(coroutine); }
+        TooltipSystem.Hide();
+    }
+
+    private IEnumerator ShowToolTip()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        TooltipSystem.Show(content, header);
+    }
+}
